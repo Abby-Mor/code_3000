f67f4a742b9f5e0fa771d7f8ce51771c62b3187d
Abby-Mor <abbyjanemorrison@gmail.com>
2026-03-06 17:47:22 -0500

    changes

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,22 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    anon_columns = set(anon_df.columns)
+    aux_columns = set(aux_df.columns)
+
+    anon_columns.discard('anon_id')
+    aux_columns.discard('name')
+
+    matching_columns = anon_columns.intersection(aux_columns)
+
+    merge = pd.merge(anon_df, aux_df, on=matching_columns, how='inner')
+    count = merge.groupby('anon_id').size()
+    unique_matches = count[count == 1].index
+
+    matches_df = merge[merge['anon_id'].isin(unique_matches)][['anon_id', 'name']]
+    matches_df.rename(columns={'name': 'matched_name'}, inplace=True)
+
+    return matches_df
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +41,6 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    total_anon_records = len(anon_df)
+    return len(matches_df) / total_anon_records if total_anon_records > 0 else 0
+
